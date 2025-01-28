dbb7fc0f6974f6168f54e5dc50e7c97db40100a5
chamane01 <148882260+chamane01@users.noreply.github.com>
2025-01-28 01:20:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import streamlit as st
+import osmnx as ox
 import folium
 from streamlit_folium import folium_static
-import geopandas as gpd
 
-# Charger le fichier GeoJSON contenant les routes extraites d'OpenStreetMap
-geojson_file = 'routeJSON.geojson'  # Remplacez par le chemin de votre fichier GeoJSON
-gdf = gpd.read_file(geojson_file)
+# Définir le nom de la ville ou les coordonnées de la zone d'intérêt
+place_name = "Botro, Côte d'Ivoire"
 
-# Obtenir les coordonnées du centre de la ville (par exemple, Botro, Côte d'Ivoire)
-center = [gdf['geometry'].y.mean(), gdf['geometry'].x.mean()]
+# Télécharger le graphe routier de la zone spécifiée
+graph = ox.graph_from_place(place_name, network_type='all')
+
+# Convertir le graphe en GeoDataFrame
+gdf_nodes, gdf_edges = ox.graph_to_gdfs(graph)
+
+# Obtenir les coordonnées du centre de la ville
+center = [gdf_nodes['y'].mean(), gdf_nodes['x'].mean()]
 
 # Créer une carte centrée sur la ville
 m = folium.Map(location=center, zoom_start=12)
 
 # Ajouter les routes à la carte
-folium.GeoJson(gdf).add_to(m)
+folium.GeoJson(gdf_edges).add_to(m)
 
-# Afficher la carte dans l'application Streamlit
+# Afficher la carte dans Streamlit
 folium_static(m)
